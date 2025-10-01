ca1ebdef20920cdd12844e29b1f3912b43c7b37f
Vipin George Abraham <georgevipin1@gmail.com>
2025-10-01 14:49:53 +0200

    Check Point 116

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,31 @@
-export default function MealDetail() {
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+export default function MealDetailsPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return (
         <>
-            <h1>Meal Details</h1>
+            <header className={styles.headder}>
+                <div className={styles.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+                    <p className={styles.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
         </>
     );
 }
---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error({ error }) {
+    return (
+        <main className="not-found">
+            <h1>Meal not found!</h1>
+            <p>Unfortunately, we could not find the requested page or reesource!</p>
+        </main>
+    );
+}
